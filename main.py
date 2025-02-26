bb16fab21c25e4f2af39340035db93d66da37e49
tonycreeney <tonycreeney@gmail.com>
2025-02-26 23:10:15 +0000

    created the whole damn things. it reads books!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import count_words
+import sys
+
+from stats import count_chars, count_words, sorted_dict
 
 
 def get_book_text(filepath):
@@ -7,12 +9,49 @@ def get_book_text(filepath):
         return file_contents
 
 
-def main():
-    frankenstein_path = "books/frankenstein.txt"
-    text = get_book_text(frankenstein_path)
+def print_list(list):
+    for item in list:
+        print(f"{item[0]}: {item[1]}")
 
+
+def title_print(book_path):
+    print("============ BOOKBOT ============")
+    print(f"Analysing book found at {book_path}...")
+
+
+def word_count(num_words):
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+
+
+def character_count(list):
+    print("--------- Character Count -------")
+    print_list(list)
+
+
+def end_print():
+    print("============= END ===============")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+    list_of_chars = count_chars(text)
     num_words = count_words(text)
-    print(num_words, "words found in the document")
+    sorted_list = sorted_dict(list_of_chars)
+
+    for char in sorted_list[:]:
+        if not char[0].isalpha():
+            sorted_list.remove(char)
+
+    title_print(book_path)
+    word_count(num_words)
+    character_count(sorted_list)
+    end_print()
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,24 @@ def count_words(book_text):
     words_of_text = book_text.split()
     num_words = len(words_of_text)
     return num_words
+
+
+def count_chars(book_text):
+    dict_of_chars = {}
+
+    for word in book_text:
+        for char in word:
+            lower_char = char.lower()
+            if lower_char not in dict_of_chars:
+                dict_of_chars.update({lower_char: 1})
+            else:
+                dict_of_chars[lower_char] += 1
+
+    return dict_of_chars
+
+
+def sorted_dict(char_dict):
+    new_list = list(char_dict.items())
+
+    new_list.sort(reverse=True, key=lambda item: item[1])
+    return new_list
